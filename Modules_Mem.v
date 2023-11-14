d07fd496977f0fbdc9337b5ad7244cb6abd77577
eric.wong <wyq5119275@yahoo.com>
2023-11-13 19:11:05 -0500

    update logic

--- Modules_Mem.v
+++ Modules_Mem.v
@@ -24,11 +24,6 @@ module InstructionMemory(
 // Array of 256 memory locations, each 8 bits wide
 reg [7:0] memory[0:255]; 
 
-// Initializing the first row of memory with zero instruction, doing nothing
-initial begin
-    memory[0:255] = 8'h00; // Default instruction is 00, nothing
-end
-
 // Memory
 always @ (addr) begin
     ins <= {memory[addr], memory[addr + 1'b1]};
--- Modules_Regs.v
+++ Modules_Regs.v
@@ -1,3 +1,4 @@
+// Code your design here
 // ---------------------------------Main Register------------------------------------------
 module MainRegister (
     input re,
@@ -8,7 +9,9 @@ module MainRegister (
     output [7:0] dout1,
     output [7:0] dout2
 );
+
 reg [7:0] regis [0:3]; // Array of 4 registers, each 8 bits wide
+
 always @ (*) begin
     if (re) begin
         regis[wd] <= din;
@@ -21,31 +24,52 @@ endmodule
 
 // ---------------------------------Stage Register-----------------------------------------
 module StageRegister (
-    input [1:0] rdi,
-    input [1:0] rd,
+    input [3:0] opi,  //operation code
+    input [1:0] rai, 
+    input [1:0] rbi,
     input [7:0] din1,
     input [7:0] din2,
+    input [7:0] immedi,
+    input [1:0] ZNi,
     input clk, // write enable bite
-    output reg[7:0] dout1,
-    output reg[7:0] dout2
+    
+    // output when negedge comes
+    output reg [3:0] opo,  
+    output reg [1:0] rao, 
+    output reg [1:0] rbo,
+    output reg [7:0] dout1,
+    output reg [7:0] dout2,
+    output reg [7:0] immedo,
+    output reg [1:0] ZNo
 );
 
 // For data holding inside of the stage register
-reg [1:0] rd, rs;
-reg [7:0] rd1, rd2;
+  reg [3:0] opt;
+  reg [1:0] rat;
+  reg [1:0] rbt;
+  reg [7:0] dt1;
+  reg [7:0] dt2;
+  reg [7:0] immedt;
+  reg [1:0] ZNt;
 
 always @(posedge clk) begin
-    rd <= ss1;
-    rs <= ss2;
-    rd1 <= din1;
-    rd2 <= din2;
+    opt <= opi;
+    rat <= rai;
+    rbt <= rbi;
+    dt1 <= din1;
+    dt2 <= din2;
+    immedt <= immedi;
+    ZNt <= ZNi;
 end
 
 always @(negedge clk) begin
-    if (we) begin
-        dout1 <= din1;
-        dout2 <= din2;
-    end
+    opo <= opt;
+    rao <= rat;
+    rbo <= rbt;
+    dout1 <= dt1;
+    dout2 <= dt2;
+    immedo <= immedt;
+    ZNo <= ZNt;
 end
 
 endmodule
